d29a77b7470d52aaf632e757ea09a87865ccbcc7
Patrik <szabo.patrik@e-vista.hu>
2019-09-16 11:45:57 +0200

    final

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+    // console.log('this.imageRef: ', this.imageRef);
+    // console.log('this.imageRef.current: ', this.imageRef.current);
+    // console.log(
+    //   'this.imageRef.current.clientHeight: ',
+    //   this.imageRef.current.clientHeight
+    // );
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans: spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,7 +3,7 @@ import './ImageList.css';
 import ImageCard from './ImageCard';
 
 const ImageList = props => {
-  const images = props.images.map(({ image }) => {
+  const images = props.images.map(image => {
     return <ImageCard key={image.id} image={image} />;
   });
 
